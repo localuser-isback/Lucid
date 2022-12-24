53c799a763918a7faea703cbc3b13321a77c3a03
pyro <pyro>
2022-12-24 14:16:01 -0600

    adding more bypasses

--- src/components/BypassCard.tsx
+++ src/components/BypassCard.tsx
@@ -3,12 +3,28 @@ import Data from "./bypass.json";
 
 function BypassCard() {
     return (
+        <>
 
+
+        
         <div className="bypassDiv1 bg-base-200">
         <div className="bypassDiv flex flex-row flex-wrap pt-6 py-6">
             {Data.map((bypass => {
                 return (
                     <>
+                <input type="checkbox" id={bypass.title} className="modal-toggle" />
+                <div className="modal">
+                <div className="modal-box w-11/12 max-w-5xl">
+                    <h3 className="font-bold text-lg">{bypass.title}</h3>
+                    <p className="py-4">{bypass.instructions}</p>
+                    <div className="mockup-code">
+                    <pre><code>{bypass.code}</code></pre>
+                    </div>
+                    <div className="modal-action">
+                    <label htmlFor={bypass.title} className="btn">Close</label>
+                    </div>
+                </div>
+                </div>
                 <div key={bypass.title}></div>
                 <div className="card w-96 bg-base-100 shadow-xl">
                 <figure><img src={bypass.image} alt={bypass.title} width="350" height="350" /></figure>
@@ -16,7 +32,7 @@ function BypassCard() {
                     <h2 className="card-title">{bypass.title}</h2>
                     <p>{bypass.description}</p>
                     <div className="card-actions justify-end">
-                    <button className="btn btn-primary">View</button>
+                    <label className="btn btn-primary" htmlFor={bypass.title}>View</label>
                     </div>
                 </div>
                 </div>
@@ -25,6 +41,7 @@ function BypassCard() {
             }))}
         </div>
         </div>
+        </>
     )
 }
 
