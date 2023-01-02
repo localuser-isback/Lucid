04688c04caa6c0a7bf483dd745cc39cabeb6a2e0
pyro <pyro>
2023-01-01 19:43:22 -0600

    image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -6,21 +6,43 @@ function GameCard() {
         <div className="gamesDiv bg-base-200">
         <div className="gamesDiv1 flex flex-row flex-wrap pt-3 py-3">
             {Data.map((game => {
-                return (
-                    <>
-                <div key={game.title}></div>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src={game.image} alt={game.title} width="350" height="350" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">{game.title}</h2>
-                    <p>{game.description}</p>
-                    <div className="card-actions justify-end">
-                    <a className="btn btn-primary" href={game.source}>Launch</a>
+
+                if (game.hasOwnProperty('image') === false) {
+                    return (
+                        <>
+                    <div key={game.title}></div>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="/images/noimage.png" alt={game.title} width="350" height="350" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{game.title}</h2>
+                        <p>{game.description}</p>
+                        <div className="card-actions justify-end">
+                        <a className="btn btn-primary" href={game.source}>Launch</a>
+                        </div>
                     </div>
-                </div>
-                </div>
-                    </>
-                )
+                    </div>
+                        </>
+                    )
+
+                } else {
+                    return (
+                        <>
+                    <div key={game.title}></div>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src={game.image} alt={game.title} width="350" height="350" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{game.title}</h2>
+                        <p>{game.description}</p>
+                        <div className="card-actions justify-end">
+                        <a className="btn btn-primary" href={game.source}>Launch</a>
+                        </div>
+                    </div>
+                    </div>
+                        </>
+                    )
+                }
+                   
+
             }))}
         </div>
         </div>
