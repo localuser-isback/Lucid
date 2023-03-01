a6378f0ae6a7d1b212e7b408469718a0361090b5
forfeited <90619153+forfeited@users.noreply.github.com>
2023-02-28 22:01:10 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,13 @@ import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
-import compress from "astro-compress";
 
 // https://astro.build/config
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown(), tailwind(), prefetch(), compress()],
+  integrations: [react(), partytown(), tailwind(), prefetch()],
   output: "server",
   adapter: node({
     mode: "middleware"
